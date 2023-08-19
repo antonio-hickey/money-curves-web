8961f8f2673ef3dea9b97768fcaaa5bed4c88571
Antonio Hickey <antoniohickey99@gmail.com>
2023-08-19 17:12:17 -0400

    feat: custom color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'text-light': '#0a1009',
+        'text-dark': '#e4eee2',
+        'background-light': '#e4eee2',
+        'background-dark': '#0a1009',
+        'primary': '#a7caa0',
+        'secondary-light': '#e4efe1',
+        'secondary-dark': '#172514',
+        'accent-light': '#2c4527',
+        'accent-dark': '#6ba560',
+      },
+    },
   },
   plugins: [],
 }
